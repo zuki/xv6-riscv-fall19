bbfa77606e55e44439af83ce132f4b34ad72f690
Keiji Suzuki <zuki.ebetsu@gmail.com>
2020-02-18 16:36:00 +0900

    add sleep.c

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int s;
+
+  if (argc != 2) {
+    fprintf(2, "Usage: sleep secs...\n");
+    exit(1);
+  }
+
+  s = atoi(argv[1]);
+  sleep(s);
+
+  exit(0);
+}
